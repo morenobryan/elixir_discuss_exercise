cc0d340dbe129cb7efc40dcf6d09f6895273ba95
Bryan Moreno <morenobryan91@gmail.com>
2017-12-30 00:55:13 -0200

    Add support for websockets and comments

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -3,9 +3,9 @@
 
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
-import {Socket} from "phoenix"
+import { Socket } from "phoenix";
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket("/socket", { params: { token: window.userToken } });
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
@@ -51,12 +51,58 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 // Finally, pass the token on connect as below. Or remove it
 // from connect if you don't care about authentication.
 
-socket.connect()
+socket.connect();
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const createSocket = topicId => {
+  // Now that you are connected, you can join channels with a topic:
+  let channel = socket.channel(`comments:${topicId}`, {});
+  channel
+    .join()
+    .receive("ok", resp => {
+      console.log(resp);
+      renderComments(resp.comments);
+    })
+    .receive("error", resp => {
+      console.log("Unable to join", resp);
+    });
 
-export default socket
+  channel.on(`comments:${topicId}:new`, renderComment);
+
+  document.querySelector("button").addEventListener("click", () => {
+    const content = document.querySelector("textarea").value;
+
+    channel.push("comment:add", { content: content });
+  });
+};
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return commentTemplate(comment);
+  });
+
+  document.querySelector(".collection").innerHTML = renderedComments.join("");
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+
+  document.querySelector(".collection").innerHTML += renderedComment;
+}
+
+function commentTemplate(comment) {
+  let email = "Anonymous";
+
+  if (comment.user) {
+    email = comment.user.email;
+  }
+  return `
+    <li class="collection-item">
+      ${comment.content}
+      <div class="secondary-content">
+        ${email}
+      </div>
+    </li>
+  `;
+}
+
+window.createSocket = createSocket;
